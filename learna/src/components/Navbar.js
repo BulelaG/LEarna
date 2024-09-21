24c21022d2781489a9c4b6a47642e4dbc5f1d625
Bulela <bulelagomoshe1@gmail.com>
2024-09-21 02:17:54 +0200

    landing css

--- learna/src/components/Navbar.js
+++ learna/src/components/Navbar.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Create a CSS file for styling
 
-const Navbar = ({ account }) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Web3 Learning Platform</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/courses">Courses</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/course/:id">Course Details</Link> {/* Adjust as needed */}
-          </li>
-        </ul>
-        <span className="navbar-text">
-          {account ? `Connected: ${account.slice(0, 6)}...${account.slice(-4)}` : 'Not Connected'}
-        </span>
+const Navbar = ({ account }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          ScholarShip
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/courses">Courses</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/course/:id">Course View</Link>
+            </li>
+          </ul>
+          {account && <span className="navbar-text ms-auto">Account: {account}</span>}
+        </div>
       </div>
-    </div>
-  </nav>
-);
-
+    </nav>
+  );
+};
 
 export default Navbar;
